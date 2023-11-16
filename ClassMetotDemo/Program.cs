cdcad37804397c44b341369f7c399ad9da44f251
bkklc <abkklc@gmail.com>
2023-11-16 17:26:03 +0300

    ClassMetotDemo

--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ClassMetotDemo;
+
+Customer customer1 = new Customer();
+customer1.Id = 1;
+customer1.FirstName = "Berk";
+customer1.LastName = "Kılıç";
+customer1.Phone = "0123 456 7899";
+
+Customer customer2 = new Customer();
+customer2.Id = 2;
+customer2.FirstName = "Ahmet";
+customer2.LastName = "Kılıç";
+customer2.Phone = "0123 456 7899";
+
+CustomerManager manager = new CustomerManager();
+manager.Add(customer1);
+manager.Update(customer1);
+manager.Delete(customer1);
+
+Console.WriteLine("------------------------------");
+
+manager.Add(customer2);
+manager.Update(customer2);
+manager.Delete(customer2);
+
+Console.WriteLine("------------------------------");
+
+
+List<Customer> customers = new List<Customer> {customer1, customer2 };
+
+foreach (var c in customers)
+{
+    manager.List(c);
+}
