a910e9ef4a7f0744fa754494b0002e533945f885
Ahmet Berk Kılıç <abkkklc@gmail.com>
2023-11-16 17:26:03 +0300

    ClassMetotDemo

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    public class CustomerManager 
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Kullanıcı Eklendi: " + customer.FirstName +" "+ customer.LastName);
+        } 
+
+        public void Update(Customer customer)
+        {
+            Console.WriteLine("Kullanıcı Güncellendi: " + customer.FirstName + " " + customer.LastName);
+
+
+        }
+
+        public void Delete(Customer customer)
+        {
+            Console.WriteLine("Kullanıcı Silindi: " + customer.FirstName + " " + customer.LastName);
+
+        }
+
+        public void List(Customer customer)
+        {
+            Console.WriteLine(customer.FirstName + " " + customer.LastName);            
+        }
+
+
+
+
+    }
+}
